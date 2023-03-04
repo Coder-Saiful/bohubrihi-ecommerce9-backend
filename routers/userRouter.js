d64797aec9380d5344d69d836c2b6cc11b516d5f
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-03-04 03:36:17 -0600

    change password routing

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { signUp, signIn } = require('../controllers/userController');
+const { signUp, signIn, changePassword } = require('../controllers/userController');
+const authorize = require('../middlewares/authorize');
 
 router.route('/signup')
     .post(signUp);
 
 router.route('/signin')
     .post(signIn);
+
+router.route('/change-password')
+    .post(authorize, changePassword);
 
 module.exports = router;
